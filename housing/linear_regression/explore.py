662b5b32fd22ad93b380dfad7649063a85efba59
Brent Hamilton <brent@arthurkn.net>
2017-05-24 17:05:48 -0700

    some of that shit

--- housing/linear_regression/explore.py
+++ housing/linear_regression/explore.py
@@ -12,33 +12,42 @@ dep_label = list(train_data.columns.values)[-1:]
 X_df = pd.get_dummies(train_data.drop(dep_label, axis=1))
 y_df = pd.get_dummies(train_data.drop(feature_labels, axis=1))
 
+print(X_df.shape)
+
 # sanitize data
 X = X_df.values
 y = y_df.values
 X[np.isfinite(X) == False] = 0
 
+model = linear_model.Lasso(alpha=17, fit_intercept=True, tol=0.001)
+model.fit(X, y)
 
-# find alpha with highest cross-validation score
-score = 0
-for i in range(1, 10000):
-  temp_model = linear_model.Ridge(alpha=i, fit_intercept=True)
-  temp_model.fit(X,y)
-  temp_score = np.mean(cross_val_score(temp_model, X, y, cv=5))
-  if temp_score > score:
-    alpha = i
-    score = temp_score
-    model = temp_model
-  else:
-    break
 
-print("using alpha:", alpha)
+# # see what we got
+# prediction = model.predict(test_X)
+# predicted_results = prediction
 
-# prepare test df (match the shape of training data)
+def getZeroedColumns(column):
+  i = np.where(X_df.columns.values == column)
+  if (model.coef_[i] == 0 and "Neighborhood_" not in column):
+    return True
+  return False
+
+to_drop = list(filter(getZeroedColumns, X_df.columns.values))
+X_df = X_df.drop(to_drop, axis=1)
+print(len(X_df.columns.values))
+
+# drop the columns that lasso had decided are butthole
+X = X_df.values
+X[np.isfinite(X) == False] = 0
+
+model = linear_model.LinearRegression()
+model.fit(X, y)
+
+# # prepare test df (match the shape of training data)
 test_X_df = pd.get_dummies(test_data).reindex(columns=X_df.columns, fill_value=0)
 test_X = test_X_df.values
 test_X[np.isfinite(test_X) == False] = 0
-
-# see what we got
 prediction = model.predict(test_X)
 predicted_results = list(map(lambda x: x[0], prediction))
 
@@ -48,4 +57,4 @@ def formatForDf(index, result):
 data = list(map(formatForDf, range(len(predicted_results)), predicted_results))
 results_df = pd.DataFrame(data)
 
-results_df.to_csv('ridge_results.csv', index=False)
+results_df.to_csv('lasso_linear_results.csv', index=False)
